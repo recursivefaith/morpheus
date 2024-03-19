cdc212d0fb875b109a27d24e66374278770f0f55
Oz Ramos <heyozramos@gmail.com>
2024-03-18 22:49:00 -0700

    #7 Adds system prompt files

--- src/morpheus/settings.ts
+++ src/morpheus/settings.ts
@@ -18,7 +18,7 @@ export default class MorpheusSettingsTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Google Gemini API key')
+			.setName('🔑 Google Gemini API key')
 			.setDesc('Grab one at: https://makersuite.google.com/app/apikey')
 			.addText(text => text
 				.setPlaceholder('Enter your key')
@@ -27,23 +27,53 @@ export default class MorpheusSettingsTab extends PluginSettingTab {
 					this.plugin.settings.geminiAPI = value;
 					await this.plugin.saveSettings();
 				}));
-
+    
+    
     new Setting(this.containerEl)
-      .setName('Select Configuration File')
-      .setDesc('Choose a file for configuration')
+      .setName('⚙️ Select System Prompt file')
+      .setDesc('Choose a file for the global system prompt, which is used to set the default voice and behavior')
       .addSearch(searchComponent => {
         new FileSuggest(this.app, searchComponent.inputEl);
         searchComponent.setPlaceholder('Type to search for a file...')
-          .setValue(this.plugin.settings.selectedFilePath)
+          .setValue(this.plugin.settings.systemPrompt)
           .onChange(async (value) => {
             // The 'value' will be the file path selected by the user from the dropdown
-            this.plugin.settings.selectedFilePath = value;
+            this.plugin.settings.systemPrompt = value;
             await this.plugin.saveSettings();
           });
       });
 
     new Setting(this.containerEl)
-      .setName('Select Skill Folder')
+      .setName('🧰 Select Skill Prompt file')
+      .setDesc('Choose a file for the global skill prompt, which is used to select skills to use')
+      .addSearch(searchComponent => {
+        new FileSuggest(this.app, searchComponent.inputEl);
+        searchComponent.setPlaceholder('Type to search for a file...')
+          .setValue(this.plugin.settings.systemPrompt)
+          .onChange(async (value) => {
+            // The 'value' will be the file path selected by the user from the dropdown
+            this.plugin.settings.systemPrompt = value;
+            await this.plugin.saveSettings();
+          });
+      });
+
+
+    new Setting(this.containerEl)
+      .setName('🗺️ Select Planning Prompt file')
+      .setDesc('Choose a file for the global planning prompt, which is used to create a plan of action')
+      .addSearch(searchComponent => {
+        new FileSuggest(this.app, searchComponent.inputEl);
+        searchComponent.setPlaceholder('Type to search for a file...')
+          .setValue(this.plugin.settings.systemPrompt)
+          .onChange(async (value) => {
+            // The 'value' will be the file path selected by the user from the dropdown
+            this.plugin.settings.systemPrompt = value;
+            await this.plugin.saveSettings();
+          });
+      });
+
+    new Setting(this.containerEl)
+      .setName('📂 Select Skill Folder')
       .setDesc('Choose the default folder for skills')
       .addSearch(searchComponent => {
         new FolderSuggest(this.app, searchComponent.inputEl);
